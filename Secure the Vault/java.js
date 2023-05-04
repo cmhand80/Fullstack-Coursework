d68a07ea604199dbc0af168046cc58f2e2c1b800
cmhand80 <chris.hand57@gmail.com>
2023-05-04 18:00:20 -0500

    updates may fourth

--- Secure the Vault/java.js
+++ Secure the Vault/java.js
@@ -1,9 +1,9 @@
 const name = "User";
-let str1 = "Hello " + name + ". You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+let str1 = "Hello " + name + ". You have been chosen to open an important vault. Here is the secret combination:";
 // Assigned gretting for user on page
-let numLock1 = 5 * 2;
-let numLock2 = 10 * 4;
-let numLock3 = 35 + 4;
+let numLock1 = 2 * 5;
+let numLock2 = 5 * 8;
+let numLock3 = 30 + 9;
 // Assigned combination, used arithmetic operators to calculate combinations for User
 alert(str1 + ' ' + numLock1 + ' ' + numLock2 + ' ' + numLock3);
 // Alerted User with greeting and combination
